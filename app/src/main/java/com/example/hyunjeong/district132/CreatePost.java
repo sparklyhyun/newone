b15568ced12d8c070387261ccdcacbb8f0b382e0
hyun\Hyun Jeong <LIMH0141@e.ntu.edu.sg>
2017-04-02 15:42:09 +0800

    connection update

--- app/src/main/java/com/example/hyunjeong/district132/CreatePost.java
+++ app/src/main/java/com/example/hyunjeong/district132/CreatePost.java
@@ -12,7 +12,10 @@ import java.util.HashMap;
 
 
 public class CreatePost extends AppCompatActivity {
-
+    int id, no_of_rooms;
+    String username, location, housetype, facilities, desc;
+    double price, size;
+    boolean purpose, furnishing;
     
 
     @Override
@@ -27,8 +30,19 @@ public class CreatePost extends AppCompatActivity {
 
         session.checkLogin();
 
-
-
-
     }
+    public void newPost(View view) {
+        PostDBHandler dbHandler = new PostDBHandler(this, null, null, 1);
+
+        //get data from user
+
+        PostDB Post = new PostDB(id, username, location, housetype, purpose, price, no_of_rooms,
+                furnishing, facilities, desc, size);
+
+        dbHandler.addPost(Post);
+    }
+}
+
+//public PostDB(int post_id, String username, String location, String housetype, boolean purpose, double price,
+//int no_of_rooms, boolean furnishing, String facilities, String desc, double size) {
 }
