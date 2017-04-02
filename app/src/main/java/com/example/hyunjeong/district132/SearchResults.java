77f6f0d516bb961bb36dd4b7feeb5ca52132d7bb
Angelynnzx <zxliew22@gmail.com>
2017-04-02 15:16:50 +0800

    Add files via upload
    displaying search results in listview

--- app/src/main/java/com/example/hyunjeong/district132/SearchResults.java
+++ app/src/main/java/com/example/hyunjeong/district132/SearchResults.java
@@ -1,8 +1,10 @@
 package com.example.hyunjeong.district132;
 
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.database.Cursor;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class SearchResults extends AppCompatActivity {
 
@@ -11,11 +13,20 @@ public class SearchResults extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_results);
     }
-    public void displayResults(Cursor posts){
 
+    public void displayResults(Cursor posts){
+        // Find ListView to populate
+        ListView lvItems = (ListView) findViewById(R.id.postID);
+
+        // Setup cursor adapter using cursor from last step
+        PostResultCursorAdapter todoAdapter = new PostResultCursorAdapter(this, posts);
+
+        // Attach cursor adapter to the ListView
+        lvItems.setAdapter(todoAdapter);
     }
 
     public void noResultsFound(){
-
+        TextView noResultsView = (TextView) findViewById(R.id.noResults);
+        noResultsView.setText("No Reults Found");
     }
 }
