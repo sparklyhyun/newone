5296fda15f1e446f8c6aebef1b917ee18288b158
Angelynnzx <zxliew22@gmail.com>
2017-04-16 14:29:09 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/hyunjeong/district132/MainActivity.java
+++ app/src/main/java/com/example/hyunjeong/district132/MainActivity.java
@@ -16,9 +16,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     String loc;
     String hse;
     String sl;
-    public static final String EXTRA_LOCATION = "com.example.hyunjeong.district132.location";
-    public static final String EXTRA_TYPE = "com.example.hyunjeong.district132.location";
-    public static final String EXTRA_PURPOSE = "com.example.hyunjeong.district132.location";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,28 +27,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         final Button searchButton = (Button) findViewById(R.id.Search);
         signInButton.setOnClickListener(this);
 
-        /*if (slease == "Sale")
-            sl = true;
-        else if (slease == "Lease")
-            sl = false;*/
-
-        /*searchButton.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                loc = location.getSelectedItem().toString();
-                hse = hseType.getSelectedItem().toString();
-                sl = saleLease.getSelectedItem().toString();
-                //TextView view = (TextView) findViewById(R.id.noResults);
-                //view.setText(loc);
-                Intent toSearchResults = new Intent(MainActivity.this, SearchResults.class);
-                toSearchResults.putExtra("location", loc);
-                toSearchResults.putExtra("housetype", hse);
-                toSearchResults.putExtra("purpose", sl);
-                startActivity(toSearchResults);
-            }
-        });*/
-
-
-
         if(session.checkLoginMain()){
             Intent alrLoggedIn = new Intent(MainActivity.this, LoggedIn.class);
             startActivity(alrLoggedIn);
@@ -59,7 +34,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     public void search(View view) {
-        // Do something in response to button
+        // collect user input to search for post
 
         final Spinner location = (Spinner) findViewById(R.id.Location);
         final Spinner hseType = (Spinner) findViewById(R.id.Type);
@@ -68,36 +43,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         hse = hseType.getSelectedItem().toString();
         sl = saleLease.getSelectedItem().toString();
 
-        /*TextView textView1 = (TextView) findViewById(R.id.textView4);
-        textView1.setText(loc);
-        TextView textView2 = (TextView) findViewById(R.id.textView6);
-        textView2.setText(hse);
-        TextView textView3 = (TextView) findViewById(R.id.textView7);
-        textView3.setText(sl);*/
-
         String[] values = new String[3];
         values[0]=loc;
         values[1]=hse;
         values[2]=sl;
 
-        /*TextView textView1 = (TextView) findViewById(R.id.textView4);
-        textView1.setText(values[0]);
-        TextView textView2 = (TextView) findViewById(R.id.textView6);
-        textView2.setText(values[1]);
-        TextView textView3 = (TextView) findViewById(R.id.textView7);
-        textView3.setText(values[2]);*/
-
         Intent intent = new Intent(this, SearchResults.class);
         intent.putExtra("array", values);
         startActivity(intent);
-
-        /*Intent intent = new Intent(this, SearchResults.class);
-        Bundle extras = new Bundle();
-        extras.putString(EXTRA_LOCATION, loc);
-        extras.putString(EXTRA_TYPE, hse);
-        extras.putString(EXTRA_PURPOSE, sl);
-        intent.putExtras(extras);
-        startActivity(intent);*/
     }
 
     public void onClick(View view) {
@@ -109,7 +62,4 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         }
     }
-
-
-
 }
